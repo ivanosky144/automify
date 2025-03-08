2e22c16e88ac7c31ad4718c95cb381d7ace8b225
ivanosky144 <ivanoskytan@gmail.com>
2025-03-08 17:12:00 +0700

    feat: add api serializer and views

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Client, Request
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
+        fields = ["id", "username", "email", "password"]
         extra_kwargs = {"password": {"write_only": True}}
     
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ["id", "name", "email", "company", "created_at", "updated_at"]
+
+class RequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Request
+        fields = ["id", "title", "description", "status", "priority", "created_at", "updated_at"]
--- server/api/views.py
+++ server/api/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, ClientSerializer, RequestSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Request
 
+class ClientListCreate(generics.CreateAPIView):
+    serializer_class = ClientSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save()   
+        else:
+            print(serializer.errors)
+
+
+class RequestListCreate(generics.CreateAPIView):
+    serializer_class = RequestSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save()   
+        else:
+            print(serializer.errors)
+
+class RequestDelete(generics.DestroyAPIView):
+    serializer_class = RequestSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Request.objects.filter()
+    
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = User
+    serializer_class = UserSerializer
     permission_classes = [AllowAny]
