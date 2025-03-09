b0737be038f0f2aba1d50c0c3dffcffb93092d15
ivanosky144 <ivanoskytan@gmail.com>
2025-03-09 14:21:41 +0700

    fix: update config and model

--- server/automify/settings.py
+++ server/automify/settings.py
@@ -94,15 +94,14 @@ WSGI_APPLICATION = 'automify.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'jSHZAylVlzglXykeFXWOPgNCHzzHKLeZ',
+        'HOST': 'gondola.proxy.rlwy.net',
+        'PORT': '17568',
     }
 }
 
